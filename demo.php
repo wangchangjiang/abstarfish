da5f2214fd7d5198cd2366d3c1dc7bab674949d4
wangchangjiang <292983934@qq.com>
2018-03-25 11:41:43 +0800

    我的第一个版本

---
+++ demo.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/25 0025
+ * Time: 上午 11:28
+ */
+require_once './vendor/autoload.php';
+use Abstarfish\Starfish\Tools;
+Tools::Hello();
---
+++ src/Abstarfish/Starfish/Tools.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/25 0025
+ * Time: 上午 11:16
+ */
+
+namespace Abstarfish\Starfish;
+
+
+class Tools{
+    public function __construct(){
+
+    }
+    public static function Hello(){
+        echo 'Hello world';
+    }
+}
