1a924d916c73a6c2742a8fd193f8f0e723b40521
wangchangjiang <292983934@qq.com>
2018-07-11 21:38:41 +0800

    添加手机号验证方法

--- src/Abstarfish/Starfish/Tools.php
+++ src/Abstarfish/Starfish/Tools.php
@@ -16,4 +16,23 @@ class Tools{
     public static function Hello(){
         echo 'Hello world! From Abstarfish!版本是多少？';
     }
+    /**
+     * 验证手机号
+     * @param $phone
+     * @return bool
+     */
+    public static function checkPhone($phone){
+        if(empty($phone)){
+            return false;
+        }
+        if(strlen($phone)!=11){
+            return false;
+        }
+        $reg = "/^1[34578]{1}\d{9}$/";
+        if (preg_match($reg, $phone)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
